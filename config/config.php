11f8816312514fd6eb595ae81b4e2a7d9701341f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-01-21 20:33:32 +0000

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,14 +1,16 @@
 <?php
-date_default_timezone_set('America/New_York');
-define("CONTACTEMAIL", "your@email.com");
+date_default_timezone_set('United Kingdom/London');
+define("CONTACTEMAIL", "g4nab.ne63@gmail.com");
 define("SVXLOGPATH", "/var/log");
 define("SVXLOGPREFIX", "svxlink");
 define("SVXCONFPATH", "/etc/svxlink");
 define("SVXCONFFILENAME", "svxlink.conf");
 define("SVXLINKPATH", "/usr/bin");
-define("SVXLOGICSECTION", "SimplexLogic");
-define("SVXMODULES", array('EchoLink', 'Parrot', 'TclVoiceMail'));
-define("TIMEZONE", "America/New_York");
+define("SVXSimplexLOGICSECTION", "SimplexLogic");
+define("SVXRepeaterLOGICSECTION", "RepeaterLogic");
+define("SVXReflectorLOGICSECTION", "ReflectorLogic");
+define("SVXMODULES", array('EchoLink', 'Parrot', 'MetarInfo', 'TclVoiceMail'));
+define("TIMEZONE", "United Kingdom/London");
 define("REFRESHAFTER", "60");
 define("SHOWPROGRESSBARS", "on");
 define("SHOWOLDMHEARD", "60");
