effed51edda5cdf0c36a7d72d271a62420432cf8
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-01-23 10:24:08 +0000

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,6 +5,10 @@ define("SVXLOGPATH", "/var/log");
 define("SVXLOGPREFIX", "svxlink");
 define("SVXCONFPATH", "/etc/svxlink");
 define("SVXCONFFILENAME", "svxlink.conf");
+define("SVXMODCONFPATH", "/etc/svxlink.d/");
+define("SVXMODECHOLINKFILENAME", "ModuleEchoLink.conf");
+define("SVXMODPARROTFILENAME", "ModuleParrot.conf");
+define("SVXMODMETARINFOFILENAME", "ModuleMetarInfo.conf");
 define("SVXLINKPATH", "/usr/bin");
 define("SVXLOGICSECTION", "SimplexLogic");
 define("SVXSimplexLOGICSECTION", "SimplexLogic");
@@ -18,5 +22,6 @@ define("SHOWOLDMHEARD", "60");
 define("TEMPERATUREALERT", "on");
 define("TEMPERATUREHIGHLEVEL", "85");
 define("SHOWQRZ", "on");
+define("SVXRESTARTSCRIPT", "restart.sh");
 ?>
 
