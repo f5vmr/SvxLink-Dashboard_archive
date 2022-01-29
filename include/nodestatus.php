538aeb50468811f6128d58bf1323e5e068023b65
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2022-01-29 18:23:33 +0000

    Update nodestatus.php

--- include/nodestatus.php
+++ include/nodestatus.php
@@ -8,7 +8,7 @@
       <table id="status" class="table table-condensed table-bordered">
         <thead>
           <tr class="text-center">
-	echo $conf;
+	echo getSvxConfig($conf);
 <!-- <?php
 	$modules = getActiveModules($logLines);
 	foreach (array_keys($modules) as $module) {
